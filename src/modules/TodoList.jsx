700099d9063b147115b77117fab14390c70a319b
asilbekali <asilbekabdugafforov7@gmail.com>
2025-06-13 16:30:42 +0500

    full finished todo

--- src/modules/TodoList.jsx
+++ src/modules/TodoList.jsx
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, setTodos }) => {
   const handleUpdateClick = (id) => {
-    alert(`Update button clicked for Todo ID: ${id}`);
+    const newValue = prompt("Update your todo:");
+    if (newValue) {
+      setTodos((prevTodos) =>
+        
+        prevTodos.map((todo) =>
+          todo.id === id ? { ...todo, value: newValue } : todo
+        )
+      );
+    }
+  };
+
+  const handleDeleteClick = (id) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  };
+
+  const handleCheckboxChange = (id) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
+      )
+    );
   };
 
   return (
     <ul className="w-[500px] flex bg-white p-5 rounded-md mx-auto mt-10 flex-col">
       {todos.map((item) => (
-        <li key={item.id} className="flex items-center">
+        <li
+          key={item.id}
+          className={`flex items-center ${
+            item.isCompleted ? "opacity-50" : ""
+          }`}
+        >
           <div className="flex items-center gap-2">
             <strong>{item.id}</strong>
             <p>{item.value}</p>
@@ -24,7 +49,10 @@ const TodoList = ({ todos }) => {
                 alt="pen"
               />
             </button>
-            <button className="duration-220 p-2 rounded-md cursor-pointer hover:bg-red-100">
+            <button
+              className="duration-220 p-2 rounded-md cursor-pointer hover:bg-red-100"
+              onClick={() => handleDeleteClick(item.id)}
+            >
               <img
                 className="w-[25px]"
                 src="src/assets/icons/trash.svg"
@@ -32,12 +60,17 @@ const TodoList = ({ todos }) => {
               />
             </button>
             <label>
-              <input className="check-input hidden" type="checkbox" />
+              <input
+                className="check-input hidden"
+                type="checkbox"
+                checked={item.isCompleted}
+                onChange={() => handleCheckboxChange(item.id)}
+              />
               <div
-                className={`check-wrapper duration-220 mb-1 cursor-pointer rounded-[100%] w-[25px] h-[25px] border-2 border-solid`}
+                className={`check-wrapper duration-220 mb-1 cursor-pointer rounded-[100%] w-[25px] h-[25px] border-2 border-solid flex items-center justify-center`}
               >
                 <img
-                  className="hidden"
+                  className={item.isCompleted ? "" : "hidden"}
                   src="src/assets/imgs/result.png"
                   alt="true"
                 />
